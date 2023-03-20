206fc405a91a8473adbffea9c59c95d3e3e71a48
WaniShivani <wanishivani4@gmail.com>
2023-03-20 19:41:13 +0530

    promiss

---
+++ promiss.js
@@ -0,0 +1,14 @@
+function updateLastUserActivityTime () {
+    const Promise=resolve('updateLastUserActivityTime')
+    return new Promise( (resolve, reject) => 
+       setTimeout(resolve,2000,'updateLastUserActivityTime')
+    )
+};
+const Promise=fetch
+('').then(res=> res.json())
+function deletePost(){
+    reject("delete the last post")
+}
+
+updateLastUserActivityTime()
+deletePost()
